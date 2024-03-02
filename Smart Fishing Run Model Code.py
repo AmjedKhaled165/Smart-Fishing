627bf17bf96c11c2ad52591d5629bbf81404378e
Amjed <122521459+AmjedKhaled165@users.noreply.github.com>
2024-03-02 17:51:47 +0200

    Smart Fishing Run Model Code.py

--- Smart Fishing Run Model Code.py
+++ Smart Fishing Run Model Code.py
@@ -5,7 +5,7 @@ import time
 
 np.set_printoptions(suppress=True)
 model = load_model("Smart Fishing Model.h5", compile=False)
-class_names = ["Fish", "Shark", "Diver", "Trash"]
+class_names = ["Diver", "Fish", "Person", "Rocks", "Shark", "Trash", "Not Fish"]
 camera = cv2.VideoCapture(0)
 
 while True:
